3faebf8dd44ee52d7bbcab2ca4371bcefbd3c796
Ilya Nozhkin <nozhkin.ii@gmail.com>
2017-07-07 11:30:28 +0300

    Revert "102 cleanup"
    This reverts commit 4e97982e3601da41e36ceb3e41ec7f0b25d8c943.

---
+++ 102/Anikin toint/Anikin toint/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Anikin toint
+//
+//  Created by Andrey Terekhov on 27/02/2017.
+//  Copyright © 2017 SE chair. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
