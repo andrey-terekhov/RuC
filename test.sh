f16a60707077a8bb8148e54fd8b972b9e367f8f0
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2017-10-19 15:43:23 +0300

    Set Travis CI

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+full_out=$1
+wait_for=1
+
 pass=0
 fail=0
 timeout=0
@@ -11,18 +14,27 @@ if ! make >/dev/null 2>/dev/null ; then
 fi
 
 
-for i in `find ./tests -name *.c`
+for code in ./tests/*.c ./tests/*/*.c ./tests/*/*/*.c
 do
-    out=`timeout 1 ./ruc $i >/dev/null 2>/dev/null`
+    out=`timeout $wait_for ./ruc $code >/dev/null 2>/dev/null`
     
     case $? in
         0)
+            if ! [[ -z $full_out ]] ; then
+                echo " build passing : $code"
+            fi
             let pass++
             ;;
         124)
+            if ! [[ -z $full_out ]] ; then
+                echo " build timeout : $code"
+            fi
             let timeout++
             ;;
         *)
+            if ! [[ -z $full_out ]] ; then
+                echo " build failing : $code"
+            fi
             let fail++
             ;;
     esac
@@ -32,6 +44,10 @@ done
 rm tree.txt codes.txt export.txt >/dev/null 2>/dev/null
 rm ruc >/dev/null 2>/dev/null
 
+
+if ! [[ -z $full_out ]] ; then
+    echo
+fi
 
 echo " pass = $pass, fail = $fail, timeout = $timeout"
 
