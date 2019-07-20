6001dc923c7ec42bfc7e0562eb9aaa59666fb964
Maxim Menshikov <maxim@menshikov.org>
2019-07-20 17:55:49 +0300

    Start refactoring main.c

--- RuC/main.c
+++ RuC/main.c
@@ -17,7 +17,7 @@ const char * name =
 #include <string.h>
 #include <wchar.h>
 #include <stdlib.h>
-
+#include <errno.h>
 #include "Defs.h"
 
 // Определение глобальных переменных
@@ -81,45 +81,17 @@ int toreprtab(char str[])
     return hashtab[hash] = oldrepr;
 }
 
-int main(int argc, const char * argv[])
+/* Первичная инициализация глобальных объектов */
+void init()
 {
-    int i;
+    memset(hashtab, 0, sizeof(hashtab));
+}
 
-    for (i=0; i<256; i++)
-        hashtab[i] = 0;
-    
-    // занесение ключевых слов в reprtab
-    keywordsnum = 1;
-    
-    input =  fopen("keywords.txt", "r");
-    if (input == NULL)
-    {
-        printf(" не найден файл %s\n", "keywords.txt");
-        exit(1);
-    }
-    getnext();
-    nextch();
-    while (scan() != LEOF)   // чтение ключевых слов
-        ;
-    fclose(input);
-    
-    if (argc < 2) {
-        input = fopen(name, "r");          //   исходный текст
-    } else {
-        input = fopen(argv[1], "r");
-    }
-    output = fopen("macro.txt", "wt");
-
-    if (input == NULL)
-    {
-        if (argc < 2) {
-            printf(" не найден файл %s\n", name);
-        } else {
-            printf(" не найден файл %s\n", argv[1]);
-        }
-        
-        exit(1);
-    }
+/* Инициализация modetab */
+void
+init_modetab()
+{
+    // занесение в modetab описателя struct {int numTh; int inf; }
     modetab[1] = 0;
     modetab[2] = MSTRUCT;
     modetab[3] = 2;
@@ -127,85 +99,188 @@ int main(int argc, const char * argv[])
     modetab[5] = modetab[7] = LINT;
     modetab[6] = toreprtab("numTh");
     modetab[8] = toreprtab("data");
-    modetab[9] = 1;                // занесение в modetab описателя struct{int numTh; int inf;}
+
+    // занесение в modetab описателя функции void t_msg_send(struct msg_info m)
+    modetab[9] = 1;
     modetab[10] = MFUNCTION;
     modetab[11] = LVOID;
     modetab[12] = 1;
     modetab[13] = 2;
-    modetab[14] = 9;               // занесение в modetab описателя  функции void t_msg_send(struct msg_info m)
+
+    // занесение в modetab описателя функции void* interpreter(void* n)
+    modetab[14] = 9;
     modetab[15] = MFUNCTION;
     modetab[16] = LVOIDASTER;
     modetab[17] = 1;
     modetab[18] = LVOIDASTER;
-    modetab[19] = startmode = 14;  // занесение в modetab описателя  функции void* interpreter(void* n)
+    modetab[19] = startmode = 14;
     md = 19;
     keywordsnum = 0;
     lines[line = 1] = 1;
     charnum = 1;
     kw = 1;
     tc = 0;
+}
 
-    printf("\nИсходный текст:\n \n");
-    preprocess_file();                //   макрогенерация
-    
-    fclose(output);
-    fclose(input);
-    
-    input  = fopen("macro.txt", "r");
-    
-    if (input == NULL)
+typedef enum ruc_io_type {
+    IO_TYPE_INPUT,
+    IO_TYPE_OUTPUT,
+} ruc_io_type;
+
+static FILE **
+io_type2file(ruc_io_type type)
+{
+    switch (type)
     {
-        printf("файл %s не найден\n", name);
+        case IO_TYPE_INPUT:
+            return &input;
+        case IO_TYPE_OUTPUT:
+            return &output;
+        default:
+            return NULL;
     }
-    if(prep_flag == 1)
+}
+
+static const char *
+io_type2access_mask(ruc_io_type type)
+{
+    switch (type)
     {
-        printf("\nТекст после препроцесора:\n \n");
+        case IO_TYPE_OUTPUT:
+            return "wt";
+        case IO_TYPE_INPUT:
+        default:
+            return "r";
+    }
+}
+
+void
+attach_io(const char *path, ruc_io_type type)
+{
+    FILE *f = fopen(path, io_type2access_mask(type));
+    FILE **target = io_type2file(type);
+
+    if (f == NULL)
+    {
+        if (type == IO_TYPE_OUTPUT)
+            printf(" ошибка открытия файла %s: %s\n", path, strerror(errno));
+        else
+            printf(" не найден файл %s\n", path);
+        exit(1);
     }
 
-    output = fopen("tree.txt", "wt");
-    
+    *target = f;
+}
+
+void
+detach_io(ruc_io_type type)
+{
+    FILE **f = io_type2file(type);
+
+    if (*f != NULL)
+        fclose(*f);
+    *f = NULL;
+}
+
+
+/* Занесение ключевых слов в reprtab */
+void
+read_keywords(const char *path)
+{
+    attach_io(path, IO_TYPE_INPUT);
+
+    keywordsnum = 1;
+    getnext();
+    nextch();
+    while (scan() != LEOF)   // чтение ключевых слов
+        ;
+
+    detach_io(IO_TYPE_INPUT);
+}
+
+/* Вывод таблиц и дерева */
+void
+output_tables_and_tree(const char *path)
+{
+    attach_io(path, IO_TYPE_OUTPUT);
+
     getnext();
     nextch();
     next = scan();
 
     ext_decl();                       //   генерация дерева
 
-    lines[line+1] = charnum;
+    lines[line + 1] = charnum;
     tablesandtree();
-    fclose(output);
-    output = fopen("codes.txt", "wt");
-    
-    codegen();                         //   генерация кода
-    
+    detach_io(IO_TYPE_OUTPUT);
+}
+
+/* Генерация кодов */
+void
+output_codes(const char *path)
+{
+    attach_io(path, IO_TYPE_OUTPUT);
+    codegen();
     tablesandcode();
-    
-    fclose(input);
-    fclose(output);
-    
-    output = fopen("export.txt", "wt");
+    detach_io(IO_TYPE_OUTPUT);
+}
+
+/* Вывод таблиц в файл */
+void
+output_export(const char *path)
+{
+    int i;
+
+    attach_io(path, IO_TYPE_OUTPUT);
     fprintf(output, "%i %i %i %i %i %i %i\n", pc, funcnum, id, rp, md, maxdisplg, wasmain);
-    
+
     for (i=0; i<pc; i++)
         fprintf(output, "%i ", mem[i]);
     fprintf(output, "\n");
-    
+
     for (i=0; i<funcnum; i++)
         fprintf(output, "%i ", functions[i]);
     fprintf(output, "\n");
-    
+
     for (i=0; i<id; i++)
         fprintf(output, "%i ", identab[i]);
     fprintf(output, "\n");
-    
+
     for (i=0; i<rp; i++)
         fprintf(output, "%i ", reprtab[i]);
-    
+
     for (i=0; i<md; i++)
         fprintf(output, "%i ", modetab[i]);
     fprintf(output, "\n");
-    
-    fclose(output);
-   
+
+    detach_io(IO_TYPE_OUTPUT);
+}
+
+int main(int argc, const char * argv[])
+{
+    init();
+    read_keywords("keywords.txt");
+
+    // Открытие исходного текста
+    attach_io(argc > 1 ? argv[1] : name, IO_TYPE_INPUT);
+
+    // Препроцессинг в файл macro.txt
+    attach_io("macro.txt", IO_TYPE_OUTPUT);
+    init_modetab();
+
+    printf("\nИсходный текст:\n \n");
+    preprocess_file();                //   макрогенерация
+
+    detach_io(IO_TYPE_OUTPUT);
+    detach_io(IO_TYPE_INPUT);
+
+    attach_io("macro.txt", IO_TYPE_INPUT);
+    output_tables_and_tree("tree.txt");
+    output_codes("codes.txt");
+    output_export("export.txt");
+
+    detach_io(IO_TYPE_INPUT);
+
     return 0;
 }
 
