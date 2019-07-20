73ae47ddbe93ee06d25d356edafd374336e1a6fa
Maxim Menshikov <maxim@menshikov.org>
2019-07-20 16:21:56 +0300

    Add SublimeGDB support

---
+++ tools/exec_wrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script wrapping execution for GDB, sourcing parameters from external script
+friendly_name=$1
+binary_path=$2
+shift 2
+test -f tmp-run-${friendly_name}.sh && . ./tmp-run-${friendly_name}.sh
+#exec -a ruc ./build/bin/ruc $@
+exec -a ${binary_path} $@ $RUC_PARAMS
