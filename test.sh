e3fc8bbe19b1f6d1cfecc269cbe1419165a71b70
Iakov Kirilenko <jake.kirilenko@gmail.com>
2019-07-28 13:05:17 +0300

    Improve CI configuration
    Make in lighter, add shellcheck, split build from test, etc

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-test_dir=$(pwd)/tests
-full_out=$1
+set -ueo pipefail
+test_dir=$PWD/tests
+full_out=${1:-}
 
 output_time=0.1
 wait_for=1
@@ -9,41 +10,32 @@ pass=0
 fail=0
 timeout=0
 
-mkdir -p build
-cd build
-cmake ..
-if ! make >/dev/null 2>/dev/null ; then
-    make
-    exit 1
-fi
-
-cd bin
+cd build/bin
 RUC=./ruc
 
 function internal_timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
-
-for code in ${test_dir}/*.c ${test_dir}/*/*.c ${test_dir}/*/*/*.c
+[[ -e "$RUC" ]]
+for code in "${test_dir}"/*.c "${test_dir}"/*/*.c "${test_dir}"/*/*/*.c
 do
-    rm -f out.txt
-    out=`internal_timeout $wait_for ${RUC} $code >out.txt 2>out.txt`
-
-    case $? in
+    RC=0
+    internal_timeout $wait_for ${RUC} "$code" &>out.txt || RC=$?
+    case $RC in
         0)
-            if ! [[ -z $full_out ]] ; then
+            if [[ -n $full_out ]] ; then
                 sleep $output_time
                 echo -e "\x1B[1;32m build passing \x1B[1;39m: $code"
             fi
-            let pass++
+	    (( ++pass ))
             ;;
         124)
-            if ! [[ -z $full_out ]] ; then
+            if [[ -n $full_out ]] ; then
                 sleep $output_time
                 echo -e "\x1B[1;34m build timeout \x1B[1;39m: $code"
             fi
-            let timeout++
+	    (( ++timeout ))
             ;;
         *)
-            if ! [[ -z $full_out ]] ; then
+            if [[ -n $full_out ]] ; then
                 sleep $output_time
                 echo -e "\x1B[1;31m build failing \x1B[1;39m: $code"
                 if [ "${DEBUG_TEST_SCRIPT:-0}" == "1" ] ; then
@@ -51,25 +43,25 @@ do
                     cat out.txt
                 fi
             fi
-            let fail++
+	    (( ++fail ))
             ;;
-    esac
+esac
 done
 
 rm -f out.txt
 
-if ! [[ -z $full_out ]] ; then
-    echo
+if [[ -n $full_out ]] ; then
+    echo ""
 fi
 
 echo -e "\x1B[1;39m pass = $pass, fail = $fail, timeout = $timeout"
 
-if ! [ $pass -eq 0 ] ; then
+if (( pass != 0 )) ; then
     exit 0
 fi
 
-if ! [ $fail -ge 0 ] ; then
-    if ! [ $timeout -ge 0 ] ; then
+if  (( fail < 0 )) ; then
+    if (( timeout < 0 )); then
         exit 0
     fi
 fi
