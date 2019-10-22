fe0c0848a2786f1d9af297d4e568a05e2103c91f
andrey-terekhov <a.terekhov@spbu.ru>
2019-10-22 17:14:46 +0300

    Update tests

---
+++ tests/mips/for2.c
@@ -0,0 +1,8 @@
+int i, a[5];
+void main()
+{
+    a[i] = i+3;
+    a[i] += i+4;
+}
+
+
--- tests/mips/for3.c
+++ tests/mips/for3.c
@@ -5,12 +5,9 @@ int f(int i)
 }
 void main()
 {
-    int c, d;
     for (i=0; i<5; ++i)
     {
-        a[i] = f(i)+2;
-        a[i] += f(i)+3;
-        c = c * d;
+        i = f(i+2)/3;
     }
 }
 
