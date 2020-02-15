dfe7aeff04e907b7eb00fcc940b6e2626361c3b0
Maxim Menshikov <maxim@menshikov.org>
2020-02-15 18:31:04 +0300

    Move scripts

--- style.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-find RuC -iname *.c -o -iname *.h | xargs clang-format -i -style=file
-find RuCVM -iname *.c -o -iname *.h | xargs clang-format -i -style=file
-find UI -iname *.c -o -iname *.h | xargs clang-format -i -style=file
-find util -iname *.c -o -iname *.h | xargs clang-format -i -style=file
