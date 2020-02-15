21b21e66c3201ebf4301c0d5ec341b5e5b2a021a
Maxim Menshikov <maxim@menshikov.org>
2020-02-15 18:42:00 +0300

    Unify tests with 'develop' branch

---
+++ tests/Fadeev/draw.c
@@ -0,0 +1,14 @@
+void main()
+{
+    pixel({0}, 1, 2);
+    line({0}, 1, 2, 3, 4);
+    rectangle({0}, 1, 2, 3, 4, 5);
+    ellipse({0}, 1, 2, 3, 4, 5);
+    clear({0});
+    draw_string({0}, 1, 2, "abc");
+    draw_number({0}, 1, 2, 3);
+    draw_number({0}, 1, 2, 3.14);
+    icon({0}, 1, 2, 3);
+}
+    
+    
