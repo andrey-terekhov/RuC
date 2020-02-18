fc35dc6a73c3d40359ba61d637b88b2468ea9da9
Maxim Menshikov <maxim@menshikov.org>
2020-02-18 23:50:49 +0300

    style.sh: fix wrong path

--- scripts/style.sh
+++ scripts/style.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find ../libs -iname *.c -o -iname *.h | xargs clang-format -i -style=file
-find ../src -iname *.c -o -iname *.h | xargs clang-format -i -style=file
+find libs -iname *.c -o -iname *.h | xargs clang-format -i -style=file
+find src -iname *.c -o -iname *.h | xargs clang-format -i -style=file
