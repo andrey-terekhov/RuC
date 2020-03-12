6bdf2f8cd70dd9a9abd08d17374b162d25792db0
Maxim Menshikov <maxim@menshikov.org>
2020-03-12 17:38:05 +0300

    style.sh: use clang-format-9 if available, or default one otherwise

--- scripts/style.sh
+++ scripts/style.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-find libs -iname *.c -o -iname *.h | xargs clang-format -i -style=file
-find src -iname *.c -o -iname *.h | xargs clang-format -i -style=file
+binary="clang-format-9"
+which "${binary}" > /dev/null
+if [ "$?" != "0" ] ; then
+	binary="clang-format"
+fi
+
+find libs -iname *.c -o -iname *.h | xargs "${binary}" -i -style=file
+find src -iname *.c -o -iname *.h | xargs "${binary}" -i -style=file
