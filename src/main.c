af2e33f408bbfd1c2d53fcc1035968bf5ccff869
andrey-terekhov <a.terekhov@spbu.ru>
2020-05-01 11:53:26 +0300

    пароль

--- src/main.c
+++ src/main.c
@@ -17,8 +17,7 @@
 #include "compiler.h"
 
 
-const char *name = "../tests/stanfunc0.c";
-	// "../tests/Egor/Macro/test3.c";
+const char *name = "../tests/Egor/Macro/test3.c";
 	// "../tests/Mishatest.c";
 	// "../tests/mips/0test.c";
 
