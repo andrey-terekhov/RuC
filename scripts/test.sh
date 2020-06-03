a7768cb430d8a1e116ad07d3b5bb9b79779953dc
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2020-06-03 22:04:19 +0300

    Update testing system for normalizing output

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,13 +2,44 @@
 
 init()
 {
-	full_out=$1
-
 	output_time=0.1
 	wait_for=1
 
-	pass=0
-	fail=0
+	while ! [[ -z $1 ]]
+	do
+		case "$1" in
+			-h|--help)
+				echo -e "Usage: ./test.sh [KEY]..."
+				echo -e "Keys:"
+				echo -e "\t-h, --help\tTo output help info"
+				echo -e "\t-s, --silence\tFor silence testing"
+				echo -e "\t-d, --debug\tSwitch on debug tracing"
+				echo -e "\t-o, --output\tSet output printing time (default = 0.1)"
+				echo -e "\t-w, --wait\tSet waiting time for timeout result (default = 1)"
+				exit 0
+				;;
+			-s|--silence)
+				silence=$1
+				;;
+			-d|--debug)
+				debug=$1
+				echo -e "In future..."
+				exit 0
+				;;
+			-o|--output)
+				output_time=$2
+				shift
+				;;
+			-w|--wait)
+				wait_for=$2
+				shift
+				;;
+		esac
+		shift
+	done
+
+	success=0
+	failure=0
 	timeout=0
 }
 
@@ -21,55 +52,83 @@ build()
 	fi
 
 	ruc_compiler=./ruc
+	
 	test_dir=../tests
+	error_dir=../tests/errors
 }
 
 internal_timeout()
 {
 	which timeout
-	if [ "$?" == "0" ] ; then
+	if [[ $? == 0 ]] ; then
 		timeout $@
 	else
-		perl -e 'alarm shift; exec @ARGV' "$@";
+		perl -e 'alarm shift; exec @ARGV' $@;
 	fi
+}
+
+message_success()
+{
+	if [[ -z $silence ]] ; then
+		echo -e "\x1B[1;32m $action success \x1B[1;39m: $code"
+		sleep $output_time
+	fi
+	let success++
+}
+
+message_timeout()
+{
+	if [[ -z $silence ]] ; then
+		echo -e "\x1B[1;34m $action timeout \x1B[1;39m: $code"
+		sleep $output_time
+	fi
+	let timeout++
+}
+
+message_failure()
+{
+	if [[ -z $silence ]] ; then
+		echo -e "\x1B[1;31m $action failure \x1B[1;39m: $code"
+		sleep $output_time
+	fi
+	let failure++
 }
 
 test()
 {
-	for code in ${test_dir}/*/*.c ${test_dir}/*/*/*.c ${test_dir}/*/*/*/*.c ${test_dir}/*/*/*/*/*.c
+	# Do not use names with spaces!
+	for code in `find ${test_dir} -name *.c`
 	do
-		out=`internal_timeout $wait_for ${ruc_compiler} $code >/dev/null 2>/dev/null`
+		action="compiling"
+		out=`internal_timeout $wait_for $ruc_compiler $code >/dev/null 2>/dev/null`
+
 		case "$?" in
 			0)
-				if ! [[ -z $full_out ]] ; then
-					sleep $output_time
-					echo -e "\x1B[1;32m build passing \x1B[1;39m: $code"
+				if [[ $code == $error_dir/* ]] ; then
+					message_failure
+				else
+					message_success
 				fi
-				let pass++
 				;;
 			124|142)
-				if ! [[ -z $full_out ]] ; then
-					sleep $output_time
-					echo -e "\x1B[1;34m build timeout \x1B[1;39m: $code"
-				fi
-				let timeout++
+				message_timeout
 				;;
 			*)
-				if ! [[ -z $full_out ]] ; then
-					sleep $output_time
-					echo -e "\x1B[1;31m build failing \x1B[1;39m: $code"
+				if [[ $code == $error_dir/* ]] ; then
+					message_success
+				else
+					message_failure
 				fi
-				let fail++
 				;;
 		esac
 	done
 
 
-	if ! [[ -z $full_out ]] ; then
+	if [[ -z $silence ]] ; then
 		echo
 	fi
 
-	echo -e "\x1B[1;39m pass = $pass, fail = $fail, timeout = $timeout"
+	echo -e "\x1B[1;39m success = $success, failure = $failure, timeout = $timeout"
 }
 
 main()
@@ -79,17 +138,11 @@ main()
 	build
 	test
 
-	if ! [ $pass -eq 0 ] ; then
-		exit 0
+	if [[ $failure != 0 || $timeout != 0 ]] ; then
+		exit 1
 	fi
 
-	if ! [ $fail -ge 0 ] ; then
-		if ! [ $timeout -ge 0 ] ; then
-			exit 0
-		fi
-	fi
-
-	exit 1
+	exit 0
 }
 
 main $@
