815a670e4e341e080bbe7b93f3b4a48dcc40e850
DIma Davladov <maestro@hu2.ru>
2020-08-28 14:58:14 +0300

    Create select_not_from_struct.c
    Некорректно считывается код после ошибки. Лечится тем, что в следующей смысловой строке после ошибки можно написать все, что угодно, и тогда дальнейший код будет считываться, пустые строки не помогают, строки с комментарием тоже

---
+++ tests/errors/Dima/select_not_from_struct.c
@@ -0,0 +1,16 @@
+struct id
+{
+	char lastname[20];   
+	char firstname[20]; 
+	int year;
+};
+
+
+int main()
+{
+	struct id structura;
+	int structu;
+	structu.year;
+	//hh // нужно раскомментить для корректного чтения дальнейшего кода
+	return 0;
+}
