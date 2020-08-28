e0f86aa7434d57f2dce2dda141c1bd61ed092b6f
DIma Davladov <maestro@hu2.ru>
2020-08-28 13:34:19 +0300

    Create wrong_struct.c
    Некорректно считывается код после ошибки. Лечится тем, что в следующей смысловой строке после ошибки можно написать все, что угодно, и тогда дальнейший код будет считываться, пустые строки не помогают, строки с комментарием тоже

---
+++ tests/errors/Dima/wrong_struct.c
@@ -0,0 +1,13 @@
+int main()
+{
+	int a;
+
+	struct int
+	// если раскоментить, то дальше код будет корректно считываться, и выдаст только необходимую ошибку
+	{
+		char lastname[20];   
+		char firstname[20]; 
+		int year;
+	};
+	return 0;
+}
