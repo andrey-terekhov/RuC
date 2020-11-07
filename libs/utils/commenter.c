3658bc56c77969384e34694a90f2f98479f89433
Dmitrii Davladov <maestro@hu2.ru>
2020-11-07 17:05:06 +0300

    Add return in commenter.c

--- libs/utils/commenter.c
+++ libs/utils/commenter.c
@@ -6,20 +6,20 @@
 
 int comment(char *const comment, const char *const path, const size_t line)
 {
-
+	return 0;
 }
 
 int macro_comment(char *const comment, const char *const path, const size_t line, const size_t symbol)
 {
-
+	return 0;
 }
 
 const char * search_comment(const char *const code, const size_t position)
 {
-
+	return '1';
 }
 
 int extract_comment(const char *const comment, char *const path, size_t *const line, size_t *const symbol)
 {
-
+	return 0;
 }
