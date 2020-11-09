adfdb7808a9fda76cae156c20138c26d2a21f3c9
Dmitrii Davladov <maestro@hu2.ru>
2020-11-10 01:56:00 +0300

    Исправлены cmt_get_****. Начата реализация поиска.

--- libs/utils/commenter.c
+++ libs/utils/commenter.c
@@ -1,11 +1,11 @@
 #include "commenter.h"
 #include <stdint.h>
 #include <stdio.h>
-//#include <string.h>
+#include <string.h>
 
 
-const char *const PREFIX = "//// #";
-const char *const SEPARATOR = " ";
+const char *const PREFIX = "// #";
+const char SEPARATOR = ' ';
 
 
 comment cmt_create(const char *const path, const size_t line)
@@ -42,16 +42,66 @@ size_t cmt_to_string(const comment *const cmt, char *const buffer)
 	
 	if (cmt->symbol != SIZE_MAX)
 	{
-		return sprintf(buffer, "%s%s%s%s%li%s%li\n", PREFIX, SEPARATOR
+		return sprintf(buffer, "%s%c%s%c%li%c%li\n", PREFIX, SEPARATOR
 			, cmt->path, SEPARATOR, cmt->line, SEPARATOR, cmt->symbol);
 	}
 
-	return sprintf(buffer, "%s%s%s%s%li\n", PREFIX, SEPARATOR, cmt->path, SEPARATOR, cmt->line);
+	return sprintf(buffer, "%s%c%s%c%li\n", PREFIX, SEPARATOR, cmt->path, SEPARATOR, cmt->line);
+}
+
+void reverse_search(comment *const cmt, const char *const code, const size_t position)
+{
+	const size_t size = strlen(PREFIX);
+	
+	size_t i = position;
+	while (i != 0)
+	{
+		if (code[i] == '\n')
+		{
+			cmt->line++;
+			i--;
+			continue;
+		}
+
+		size_t j = 0;
+		while (j < size && PREFIX[size - j - 1] == code[i - j])
+		{
+			j++;
+		}
+
+		if (j == size)
+		{
+			cmt->path = &code[i + 2];
+			break;
+		}
+
+		i--;
+	}
 }
 
 comment cmt_search(const char *const code, const size_t position)
 {
 	comment cmt;
+	cmt.path = NULL;
+	cmt.line = 0;
+	cmt.symbol = SIZE_MAX;
+	cmt.code = NULL;
+	
+	if (code == NULL)
+	{
+		return cmt;
+	}
+	
+	size_t i = position;
+	while (i != 0 && code[i - 1] != '\n')
+	{
+		i--;
+	}
+
+	cmt.code = &code[i];
+	cmt.symbol = position - i;
+
+	reverse_search(&cmt, code, i);
 	return cmt;
 }
 
@@ -64,12 +114,9 @@ int cmt_is_correct(const comment *const cmt)
 
 size_t cmt_get_tag(const comment *const cmt, char *const buffer)
 {
-	if (!cmt_is_correct(cmt) || buffer == NULL)
-	{
-		return 0;
-	}
+	size_t i = cmt_get_path(cmt, buffer);
 
-	return sprintf(buffer, "%s:%li:%li", cmt->path, cmt->line, cmt->symbol);
+	return i == 0 ? 0 : i + sprintf(&buffer[i], ":%li:%li", cmt->line, cmt->symbol);
 }
 
 size_t cmt_get_code_line(const comment *const cmt, char *const buffer)
@@ -97,7 +144,15 @@ size_t cmt_get_path(const comment *const cmt, char *const buffer)
 		return 0;
 	}
 
-	return sprintf(buffer, "%s", cmt->path);
+	size_t i = 0;
+	while (cmt->path[i] != '\0' && cmt->path[i] != '\n' && cmt->path[i] != SEPARATOR)
+	{
+		buffer[i] = cmt->path[i];
+		i++;
+	}
+
+	buffer[i] = '\0';
+	return i;
 }
 
 size_t cmt_get_line(const comment *const cmt)
