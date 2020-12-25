0905771429b12112a3556d8c8ceda3af9cd4039b
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2020-12-25 16:54:03 +0300

    mem_set and mem_add in codegen

--- libs/compiler/codegen.c
+++ libs/compiler/codegen.c
@@ -318,9 +318,9 @@ int Expr_gen(syntax *const sx, int incond)
 					adelse = sx->pc++;
 					Expr_gen(sx, 0); // then
 					tocode(sx, B);
-					sx->mem[sx->pc] = ad;
-					ad = sx->pc;
-					sx->mem[adelse] = ++sx->pc;
+					mem_add(sx, ad);
+					ad = sx->pc - 1;
+					mem_set(sx, adelse, sx->pc - 1);
 					Expr_gen(sx, 1); // else или cond
 				} while (sx->tree[sx->tc] == TCondexpr);
 
