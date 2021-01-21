b818764ea74c1655a1fa1b8be103c2942b8ad731
6easy9 <maestro@hu2.ru>
2021-01-21 22:17:35 +0300

    Реализованы vector_create, vector_size, vector_is_correct, vector_clear

--- libs/utils/vector.c
+++ libs/utils/vector.c
@@ -26,7 +26,16 @@
  */
 
 
-vector vector_create(const size_t values);
+vector vector_create(const size_t values)
+{
+	vector vec;
+
+	vec.size = 0;
+	vec.size_alloc = values != 0 ? values : 1;
+	vec.array = malloc(vec.size_alloc * sizeof(int64_t));
+
+	return vec;
+}
 
 
 size_t map_add(vector *const vec, const int64_t value);
@@ -36,9 +45,26 @@ int vector_set(vector *const vec, const size_t index, const int64_t value);
 int64_t vector_get(const vector *const vec, const size_t index);
 
 
-size_t vector_size(vector *const vec);
+size_t vector_size(vector *const vec)
+{
+	return vector_is_correct(vec) ? vec->size : SIZE_MAX;
+}
 
-int vector_is_correct(const vector *const vec);
+int vector_is_correct(const vector *const vec)
+{
+	return vec != NULL && vec->array != NULL;
+}
 
 
-int vector_clear(vector *const vec);
+int vector_clear(vector *const vec)
+{
+	if (!vector_is_correct(vec))
+	{
+		return -1;
+	}
+
+	free(vec->array);
+	vec->array = NULL;
+
+	return 0;
+}
