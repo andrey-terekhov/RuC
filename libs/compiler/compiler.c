8c3798ff0630691f09094c0e7b79fa3599fe1692
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2021-03-11 18:31:14 +0300

    Add defaults for LLVM & MIPS

--- libs/compiler/compiler.c
+++ libs/compiler/compiler.c
@@ -33,6 +33,8 @@
 const char *const DEFAULT_MACRO = "macro.txt";
 
 const char *const DEFAULT_VM = "out.ruc";
+const char *const DEFAULT_LLVM = "out.ll";
+const char *const DEFAULT_MIPS = "out.s";
 
 
 typedef int (*encoder)(const workspace *const ws, universal_io *const io, syntax *const sx);
