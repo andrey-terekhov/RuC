791a47b3dd2460f9b6a65fda9944b4d941175103
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2021-03-15 16:52:35 +0300

    expr

--- libs/compiler/llvmgen.c
+++ libs/compiler/llvmgen.c
@@ -15,6 +15,7 @@
  */
 
 #include "llvmgen.h"
+#include "codes.h"
 #include "errors.h"
 #include "llvmopt.h"
 #include "tree.h"
@@ -471,6 +472,7 @@ int encode_to_llvm(const workspace *const ws, universal_io *const io, syntax *co
 	{
 		return -1;
 	}
+	tree_print("new1.txt", &(sx->tree));
 
 	return codegen(io, sx);
 }
--- libs/compiler/llvmopt.c
+++ libs/compiler/llvmopt.c
@@ -15,10 +15,24 @@
  */
 
 #include "llvmopt.h"
+#include "codes.h"
 #include "tree.h"
 #include "uniprinter.h"
 #include <string.h>
 
+
+typedef struct expr_node_info
+{
+	node *parent;						/**< Родитель узла */ //проблема в указателе при выходе из node_recursive память очищается и он теряется
+	size_t child_num;					/**< Номер ребёнка, которым является узел */
+	size_t node_num;					/**< Количество узлов после данного узла при перестановке */
+} expr_node_info;
+
+typedef struct expr_stack
+{
+	expr_node_info stack[512];			/**< Стек */
+	size_t stack_size;					/**< Размер стека */
+} expr_stack;
 
 typedef struct information
 {
@@ -26,11 +40,54 @@ typedef struct information
 
 	item_t string_num;					/**< Номер строки */
 	item_t was_printf;					/**< Флаг наличия printf в исходном коде */
+	expr_stack stack;					/**< Стек для преобразования выражений */
 } information;
 
 
-static void node_recursive(information *const info, node *const nd)
+static void expr_stack_push(information *const info, expr_node_info const expr_nd)
 {
+	info->stack.stack[info->stack.stack_size++] = expr_nd;
+}
+
+static expr_node_info expr_stack_pop(information *const info)
+{
+	return info->stack.stack[--info->stack.stack_size];
+}
+
+static void expr_stack_clear(information *const info)
+{
+	info->stack.stack_size = 0;
+}
+
+static int is_operand(node *const nd)
+{
+	switch (node_get_type(nd))
+	{
+		case TIdent:
+		case TIdenttoval:
+		case TConst:
+			return 1;
+	}
+
+	return 0;
+}
+
+static int is_expression(node *const nd)
+{
+	switch (node_get_type(nd))
+	{
+		// бинарные операции
+		case ASSV:
+		case LPLUS:
+			return 2;
+	}
+
+	return 0;
+}
+
+static void node_recursive(information *const info, node *const nd, syntax *const sx)
+{
+	printf("children = %zi tc = %zi argc = %zi type = %zi\n", nd->children, nd->type, nd->argc, node_get_type(nd));
 	for (size_t i = 0; i < node_get_amount(nd); i++)
 	{
 		node child = node_get_child(nd, i);
@@ -80,7 +137,85 @@ static void node_recursive(information *const info, node *const nd)
 			break;
 		}
 
-		node_recursive(info, &child);
+		// перестановка узлов выражений
+		if (is_operand(&child))
+		{
+			expr_node_info node_info = {nd, i, 1};
+			node_copy(node_info.parent, nd);
+			expr_stack_push(info, node_info);
+		}
+		else if (is_expression(&child) == 2)
+		{
+			expr_node_info second_operand = expr_stack_pop(info);
+			expr_node_info first_operand = expr_stack_pop(info);
+
+			// отладочные печати -- стек работает исправно
+			// node child_second = node_get_child(second_operand.parent, second_operand.child_num); 
+			// printf("child_second %zi\n", child_second.children);
+			// node child_first = node_get_child(first_operand.parent, first_operand.child_num); 
+			// printf("child_first %zi\n", child_first.children);
+			// printf("operation %zi\n", child.children);
+
+			// это работает только для простых выражений, нужно задействовать ещё node_num
+			printf("\nchange1:\n");
+			printf("first node parent to swap: children = %zi tc = %zi argc = %zi type = %zi child_num = %zi\n", 
+				nd->children, nd->type, nd->argc, node_get_type(nd), i);
+			node child1 = node_get_child(nd, i);
+			printf("first node child to swap: tc = %zi argc = %zi type = %zi \n", 
+				child1.type, child1.argc, node_get_type(&child1));
+			printf("second node parent to swap: children = %zi tc = %zi argc = %zi type = %zi child_num = %zi\n", 
+				second_operand.parent->children, second_operand.parent->type, second_operand.parent->argc, 
+				node_get_type(second_operand.parent), second_operand.child_num);
+			node child2 = node_get_child(second_operand.parent, second_operand.child_num);
+			printf("second node child to swap: tc = %zi argc = %zi type = %zi \n", 
+				child2.type, child2.argc, node_get_type(&child2));
+
+			node_order(nd, i, second_operand.parent, second_operand.child_num);
+
+			printf("after change1:\n");
+			printf("first node parent to swap: children = %zi tc = %zi argc = %zi type = %zi child_num = %zi\n", 
+				nd->children, nd->type, nd->argc, node_get_type(nd), i);
+			node child11 = node_get_child(nd, i);
+			printf("first node child to swap: tc = %zi argc = %zi type = %zi \n", 
+				child11.type, child11.argc, node_get_type(&child11));
+			printf("second parent node to swap: children = %zi tc = %zi argc = %zi type = %zi child_num = %zi\n", 
+				second_operand.parent->children, second_operand.parent->type, second_operand.parent->argc, 
+				node_get_type(second_operand.parent), second_operand.child_num);
+			node child22 = node_get_child(second_operand.parent, second_operand.child_num);
+			printf("second node child to swap: children = %zi tc = %zi argc = %zi type = %zi \n", 
+				child22.children, child.type, child22.argc, node_get_type(&child22));
+
+			// эспериментально-отладочный if
+			// if (second_operand.node_num == 3)
+			// {
+			// 	printf("first node to swap: children = %zi tc = %zi argc = %zi type = %zi\n", 
+			// 		nd->children, nd->type, nd->argc, node_get_type(nd));
+			// 	node child1 = node_get_child(second_operand.parent, second_operand.child_num); 
+			// 	printf("second node to swap: children = %zi tc = %zi argc = %zi type = %zi\n", 
+			// 		child1.children, child1.type, child1.argc, node_get_type(&child1));
+			// 	tree_print("new01.txt", &(sx->tree));
+			// 	// очень странные дела происходят
+			// 	// node_order(nd, 0, &child1, 0); // в выражениях есть только один потомок
+			// 	// for (size_t tc = 0; tc < sx->tree.size; tc++)
+			// 	// 	printf("tc=%zi, %" PRIitem "\n", tc, sx->tree.array[tc]);
+			// 	// node child2 = node_get_child(&child1, 0); // в выражениях есть только один потомок
+			// 	// printf("child2 %i\n", child2.children);
+			// 	// node_order(nd, i, &child2, 0); // в выражениях есть только один потомок
+			// }
+
+			printf("\nchange2\n");
+			node_order(second_operand.parent, second_operand.child_num, first_operand.parent, first_operand.child_num);
+
+			first_operand.node_num = first_operand.node_num + second_operand.node_num + 1;
+			expr_stack_push(info, first_operand);
+		}
+
+		node_recursive(info, &child, sx);
+
+		if (node_get_type(nd))
+		{
+			expr_stack_clear(info);
+		}
 	}
 }
 
@@ -90,12 +225,13 @@ static int optimize_pass(universal_io *const io, syntax *const sx)
 	info.io = io;
 	info.string_num = 1;
 	info.was_printf = 0;
+	info.stack.stack_size = 0;
 
 	node nd = node_get_root(&sx->tree);
 	for (size_t i = 0; i < node_get_amount(&nd); i++)
 	{
 		node child = node_get_child(&nd, i);
-		node_recursive(&info, &child);
+		node_recursive(&info, &child, sx);
 	}
 
 	uni_printf(io, "\n");
