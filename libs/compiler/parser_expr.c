ad76822e192d381f553f4328c6198884adbaa033
bugdea1er <bugdealer@icloud.com>
2021-04-08 14:20:38 +0300

    Фикс для компиляции на windows

--- libs/compiler/parser_expr.c
+++ libs/compiler/parser_expr.c
@@ -1415,7 +1415,8 @@ void parse_subexpression(parser *const prs)
 			vector_increase(&TREE, 1);
 		}
 
-		prs->stackop[prs->sp++] = (operator_t){precedence, prs->token, addr};
+		operator_t operator = (operator_t){precedence, prs->token, addr};
+		prs->stackop[prs->sp++] = operator;
 		token_consume(prs);
 		parse_unary_expression(prs);
 		precedence = operator_precedence(prs->token);
