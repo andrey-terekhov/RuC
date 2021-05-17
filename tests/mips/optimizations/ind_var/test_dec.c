2f8cb9c2bdcff86834295969ef24433b7248c76a
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2021-05-17 11:47:34 +0300

    tests

---
+++ tests/mips/optimizations/ind_var/test_dec.c
@@ -0,0 +1,14 @@
+
+void main()
+{
+	register int i;
+	int a[5];
+
+	for (i = 4; i >= 0; --i)
+	{
+		a[i] = i;
+	}
+	printid(a);
+}
+
+
---
+++ tests/mips/optimizations/ind_var/test_dyn.c
@@ -0,0 +1,15 @@
+
+void main()
+{
+	register int i;
+	int m = 5;
+	int a[m];
+
+	for (i = 0; i < 5; ++i)
+	{
+		a[i] = i;
+	}
+	printid(a);
+}
+
+
