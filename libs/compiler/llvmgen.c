c7072f5a79d397fa65afe6b84bfe0880787a8053
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2021-05-19 16:21:45 +0300

    debug

--- libs/compiler/llvmgen.c
+++ libs/compiler/llvmgen.c
@@ -15,7 +15,6 @@
  */
 
 #include "llvmgen.h"
-#include "codes.h"
 #include "errors.h"
 #include "llvmopt.h"
 #include "tree.h"
@@ -1432,7 +1431,6 @@ int encode_to_llvm(const workspace *const ws, universal_io *const io, syntax *co
 	{
 		return -1;
 	}
-	tree_print("new1.txt", &(sx->tree));
 
 	return codegen(io, sx);
 }
