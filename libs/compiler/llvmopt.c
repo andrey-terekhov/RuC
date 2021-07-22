45ca6d700ba27ca31598e449551093f287e7bf05
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2021-07-22 15:48:29 +0300

    debug check in transposition

--- libs/compiler/llvmopt.c
+++ libs/compiler/llvmopt.c
@@ -58,10 +58,9 @@ static inline void stack_resize(information *const info, const size_t size)
 }
 
 
-static int transposition(node *const expr, const size_t expr_depth, node *const cur, const size_t cur_depth)
+static int transposition(node *const expr, const size_t expr_depth, node *const cur)
 {
-	if (!node_is_correct(expr) || !node_is_correct(cur)
-		|| expr_depth == SIZE_MAX || cur_depth == SIZE_MAX)
+	if (!node_is_correct(expr) || !node_is_correct(cur))
 	{
 		system_error(transposition_not_possible);
 		return -1;
@@ -385,7 +384,7 @@ static int node_recursive(information *const info, node *const nd)
 
 						// перестановка с операндом
 						node operand = node_load(&info->sx->tree, (size_t)index);
-						has_error |= transposition(&operand, (size_t)operand_depth, &child, 1);
+						has_error |= transposition(&operand, (size_t)operand_depth, &child);
 						operand_depth++;
 
 						if (node_get_type(&operand) == OP_CALL1)
@@ -422,7 +421,7 @@ static int node_recursive(information *const info, node *const nd)
 
 						// перестановка со вторым операндом
 						node second_node = node_load(&info->sx->tree, (size_t)second_index);
-						has_error |= transposition(&second_node, (size_t)second_depth, &child, 1);
+						has_error |= transposition(&second_node, (size_t)second_depth, &child);
 						second_depth++;
 
 						parent = node_get_parent(&second_node);
@@ -435,7 +434,7 @@ static int node_recursive(information *const info, node *const nd)
 
 						// перестановка с первым операндом
 						node first_node = node_load(&info->sx->tree, (size_t)first_index);
-						has_error |= transposition(&first_node, (size_t)first_depth, &second_node, (size_t)second_depth);
+						has_error |= transposition(&first_node, (size_t)first_depth, &second_node);
 						first_depth += second_depth;
 
 						// добавляем в стек переставленное выражение
