16e9447587e0a83ac4dac99e0f06a3ea1cb2a2c1
6easy9 <maestro@hu2.ru>
2021-07-24 01:53:35 +0300

    Реализованы функции

--- libs/utils/strings.c
+++ libs/utils/strings.c
@@ -21,6 +21,52 @@
 static const size_t AVERAGE_STRING_SIZE = 256;
 
 
+static inline int strings_add_index(strings *const vec)
+{
+	if (vec->indexes_size == vec->indexes_alloc)
+	{
+		size_t *indexes_new = realloc(vec->indexes, 2 * vec->indexes_alloc * sizeof(size_t));
+		if (indexes_new == NULL)
+		{
+			return -1;
+		}
+
+		vec->indexes_alloc *= 2;
+		vec->indexes = indexes_new;
+	}
+
+	vec->indexes[vec->indexes_size] = vec->all_strings_size;
+	return 0;
+}
+
+static inline int strings_increase(strings *const vec, const size_t size)
+{
+	if (vec->all_strings_size + size <= vec->all_strings_alloc)
+	{
+		return 0;
+	}
+
+	char *all_strings_new = realloc(vec->all_strings, 2 * vec->all_strings_alloc * sizeof(char));
+	if (all_strings_new == NULL)
+	{
+		return -1;
+	}
+
+	vec->all_strings_alloc *= 2;
+	vec->all_strings = all_strings_new;
+	return strings_increase(vec, size);
+}
+
+
+/*
+ *	 __     __   __     ______   ______     ______     ______   ______     ______     ______
+ *	/\ \   /\ "-.\ \   /\__  _\ /\  ___\   /\  == \   /\  ___\ /\  __ \   /\  ___\   /\  ___\
+ *	\ \ \  \ \ \-.  \  \/_/\ \/ \ \  __\   \ \  __<   \ \  __\ \ \  __ \  \ \ \____  \ \  __\
+ *	 \ \_\  \ \_\\"\_\    \ \_\  \ \_____\  \ \_\ \_\  \ \_\    \ \_\ \_\  \ \_____\  \ \_____\
+ *	  \/_/   \/_/ \/_/     \/_/   \/_____/   \/_/ /_/   \/_/     \/_/\/_/   \/_____/   \/_____/
+ */
+
+
 strings strings_create(const size_t alloc)
 {
 	strings vec;
@@ -50,49 +96,67 @@ strings strings_create(const size_t alloc)
 
 size_t strings_add(strings *const vec, const char *const str)
 {
-	if (!strings_is_correct(vec) || str == NULL)
+	if (!strings_is_correct(vec) || str == NULL || str[0] == '\0' || strings_add_index(vec))
 	{
 		return SIZE_MAX;
 	}
 
-	if (vec->indexes_size == vec->indexes_alloc)
+	for (size_t i = 0; str[i] != '\0'; i++)
 	{
-		size_t *indexes_new = realloc(vec->indexes, 2 * vec->indexes_alloc * sizeof(size_t));
-		if (indexes_new == NULL)
+		if (strings_increase(vec, 2))
 		{
 			return SIZE_MAX;
 		}
 
-		vec->indexes_alloc *= 2;
-		vec->indexes = indexes_new;
+		vec->all_strings[vec->all_strings_size++] = str[i];
 	}
 
-	vec->indexes[vec->indexes_size] = vec->all_strings_size;
-	
-	size_t i = 0;
-	do
-	{
-		if (vec->all_strings_size == vec->all_strings_alloc)
-		{
-			char *all_strings_new = realloc(vec->all_strings, 2 * vec->all_strings_alloc * sizeof(char));
-			if (all_strings_new == NULL)
-			{
-				return SIZE_MAX;
-			}
-
-			vec->all_strings_alloc *= 2;
-			vec->all_strings = all_strings_new;
-		}
-
-		vec->all_strings[vec->all_strings_size++] = str[i];
-	} while (str[i++] != '\0');
-
+	vec->all_strings[vec->all_strings_size++] = '\0';
 	return vec->indexes_size++;
 }
 
-size_t strings_add_by_utf8(strings *const vec, const char32_t *const str);
+size_t strings_add_by_utf8(strings *const vec, const char32_t *const str)
+{
+	if (!strings_is_correct(vec) || str == NULL || str[0] == '\0' || strings_add_index(vec))
+	{
+		return SIZE_MAX;
+	}
 
-size_t strings_add_by_vector(strings *const vec, const vector *const str);
+	for (size_t i = 0; str[i] != '\0'; i++)
+	{
+		if (strings_increase(vec, utf8_size(str[i]) + 1))
+		{
+			return SIZE_MAX;
+		}
+
+		vec->all_strings_size += utf8_to_string(&vec->all_strings[vec->all_strings_size], str[i]);
+	}
+
+	vec->all_strings_size++;
+	return vec->indexes_size++;
+}
+
+size_t strings_add_by_vector(strings *const vec, const vector *const str)
+{
+	if (!strings_is_correct(vec) || !vector_is_correct(str) || vector_get(str, 0) == '\0' || strings_add_index(vec))
+	{
+		return SIZE_MAX;
+	}
+
+	for (size_t i = 0; i < vector_size(str); i++)
+	{
+		const char32_t ch = (char32_t)vector_get(str, i);
+		if (strings_increase(vec, utf8_size(ch) + 1))
+		{
+			return SIZE_MAX;
+		}
+
+		vec->all_strings_size += utf8_to_string(&vec->all_strings[vec->all_strings_size], ch);
+	}
+
+	vec->all_strings_size++;
+	return vec->indexes_size++;
+}
 
 
 const char *strings_get(const strings *const vec, const size_t index)
