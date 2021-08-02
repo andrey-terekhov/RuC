df6454bb8edb0142b69613b2f3cf70c01c349eec
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2021-08-02 13:27:16 +0300

    добавлены заготовки кодогенератора llvm

---
+++ libs/compiler/llvmopt.c
@@ -0,0 +1,44 @@
+/*
+ *	Copyright 2021 Andrey Terekhov, Ivan S. Arkhipov
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+
+#include "llvmopt.h"
+#include <string.h>
+#include "errors.h"
+#include "operations.h"
+#include "parser.h"
+#include "stack.h"
+#include "tree.h"
+#include "uniprinter.h"
+
+
+/*
+ *	 __	 __   __	 ______   ______	 ______	 ______   ______	 ______	 ______
+ *	/\ \   /\ "-.\ \   /\__  _\ /\  ___\   /\  == \   /\  ___\ /\  __ \   /\  ___\   /\  ___\
+ *	\ \ \  \ \ \-.  \  \/_/\ \/ \ \  __\   \ \  __<   \ \  __\ \ \  __ \  \ \ \____  \ \  __\
+ *	 \ \_\  \ \_\\"\_\	\ \_\  \ \_____\  \ \_\ \_\  \ \_\	\ \_\ \_\  \ \_____\  \ \_____\
+ *	  \/_/   \/_/ \/_/	 \/_/   \/_____/   \/_/ /_/   \/_/	 \/_/\/_/   \/_____/   \/_____/
+ */
+
+
+int optimize_for_llvm(const workspace *const ws, syntax *const sx)
+{
+	if (!ws_is_correct(ws) || sx == NULL)
+	{
+		return -1;
+	}
+
+	return 0;
+}
