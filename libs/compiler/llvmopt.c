5f33b37c16f207609d246f235a721e718840a30c
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2021-08-12 12:37:32 +0300

    check

--- libs/compiler/llvmopt.c
+++ libs/compiler/llvmopt.c
@@ -116,7 +116,8 @@ static int node_recursive(information *const info, node *const nd)
 					}
 					else
 					{
-						const double num = to_double(node_get_arg(&child, 2), node_get_arg(&child, 3));
+						// const double num = to_double(node_get_arg(&child, 2), node_get_arg(&child, 3));
+						const double num = 1.1;
 						uni_printf(info->sx->io, "double %f%s", num
 							, i < node_get_amount(nd) - 1 ? ", " : "], align 8\n");
 					}
