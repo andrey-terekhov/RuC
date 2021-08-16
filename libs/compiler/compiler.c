4f237b5a9a0bb1e303001ef92777bdbd1b9e9621
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2021-08-16 12:51:50 +0300

    добавлены файлы для кодогенерации

--- libs/compiler/compiler.c
+++ libs/compiler/compiler.c
@@ -19,7 +19,7 @@
 #include <string.h>
 #include "codegen.h"
 #include "errors.h"
-// #include "llvmgen.h"
+#include "mipsgen.h"
 #include "parser.h"
 #include "preprocessor.h"
 #include "syntax.h"
@@ -139,10 +139,10 @@ int compile(workspace *const ws)
 		{
 			return compile_to_vm(ws);
 		}
-		// else if (strcmp(flag, "-LLVM") == 0)
-		// {
-		// 	return compile_to_llvm(ws);
-		// }
+		else if (strcmp(flag, "-mips") == 0)
+		{
+			return compile_to_mips(ws);
+		}
 	}
 }
 
@@ -162,15 +162,15 @@ int compile_to_vm(workspace *const ws)
 	return ret;
 }
 
-// int compile_to_llvm(workspace *const ws)
-// {
-// 	if (ws_get_output(ws) == NULL)
-// 	{
-// 		ws_set_output(ws, DEFAULT_LLVM);
-// 	}
+int compile_to_mips(workspace *const ws)
+{
+	if (ws_get_output(ws) == NULL)
+	{
+		ws_set_output(ws, DEFAULT_LLVM);
+	}
 
-// 	return compile_from_ws(ws, &encode_to_llvm);
-// }
+	return compile_from_ws(ws, &encode_to_mips);
+}
 
 
 
@@ -186,11 +186,11 @@ int auto_compile_to_vm(const int argc, const char *const *const argv)
 	return compile_to_vm(&ws);
 }
 
-// int auto_compile_to_llvm(const int argc, const char *const *const argv)
-// {
-// 	workspace ws = ws_parse_args(argc, argv);
-// 	return compile_to_llvm(&ws);
-// }
+int auto_compile_to_mips(const int argc, const char *const *const argv)
+{
+	workspace ws = ws_parse_args(argc, argv);
+	return compile_to_mips(&ws);
+}
 
 
 int no_macro_compile_to_vm(const char *const path)
@@ -212,15 +212,15 @@ int no_macro_compile_to_vm(const char *const path)
 	return ret;
 }
 
-// int no_macro_compile_to_llvm(const char *const path)
-// {
-// 	universal_io io = io_create();
-// 	in_set_file(&io, path);
+int no_macro_compile_to_mips(const char *const path)
+{
+	universal_io io = io_create();
+	in_set_file(&io, path);
 
-// 	workspace ws = ws_create();
-// 	ws_add_file(&ws, path);
-// 	ws_set_output(&ws, DEFAULT_LLVM);
-// 	out_set_file(&io, ws_get_output(&ws));
+	workspace ws = ws_create();
+	ws_add_file(&ws, path);
+	ws_set_output(&ws, DEFAULT_LLVM);
+	out_set_file(&io, ws_get_output(&ws));
 
-// 	return compile_from_io(&ws, &io, &encode_to_llvm);
-// }
+	return compile_from_io(&ws, &io, &encode_to_mips);
+}
