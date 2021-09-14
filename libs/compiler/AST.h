13c8591fa8df7b903cbe57fbc34572e5496b38be
bugdea1er <bugdealer@icloud.com>
2021-09-14 16:53:54 +0300

    Добавлена функция expression_call_get_arguments_amount()

--- libs/compiler/AST.h
+++ libs/compiler/AST.h
@@ -223,6 +223,18 @@ inline node expression_call_get_callee(const node *const nd)
 }
 
 /**
+ *	Get arguments amount of call expression
+ *
+ *	@param	nd		Call expression
+ *
+ *	@return	Arguments amount
+ */
+inline size_t expression_call_get_arguments_amount(const node *const nd)
+{
+	return node_get_type(nd) == OP_CALL ? node_get_amount(nd) - 1 : SIZE_MAX;
+}
+
+/**
  *	Get argument of call expression by index
  *
  *	@param	nd		Call expression
