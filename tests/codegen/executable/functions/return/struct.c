74b1f2cfe788db0bbb5ddc387ad449ee8c5e7267
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2022-03-10 05:59:30 +0300

    fix

--- tests/codegen/executable/functions/return/struct.c
+++ tests/codegen/executable/functions/return/struct.c
@@ -2,14 +2,14 @@ struct point{float x; float y;} p = {1, 2};
 
 struct point f(struct point pa, struct point pb, struct point pc)
 {
-    assert(abs(pa.x - 1) < 0.000001, "p.x must be 1");
-    assert(abs(pa.y - 2) < 0.000001, "p.y must be 2");
+    assert(pa.x == 1, "pa.x must be 1");
+    assert(pa.y == 2, "pa.y must be 2");
 
     pb.x = 1.1;
     pb.y = 2.2;
 
-    assert(abs(pb.x - 1.1) < 0.000001, "pd.x must be 1.1");
-    assert(abs(pb.y - 2.2) < 0.000001, "pd.y must be 2.2");
+    assert(pb.x == 1.1, "pb.x must be 1.1");
+    assert(pb.y == 2.2, "pb.y must be 2.2");
 
     return pb;
 }
@@ -18,11 +18,11 @@ void main()
 {
     struct point pd;
 
-    assert(abs(p.x - 1) < 0.000001, "p.x must be 1");
-    assert(abs(p.y - 2) < 0.000001, "p.y must be 2");
+    assert(p.x == 1, "p.x must be 1");
+    assert(p.y == 2, "p.y must be 2");
 
     pd = f(p, p, p);
 
-    assert(abs(pd.x - 1.1) < 0.000001, "pd.x must be 1.1");
-    assert(abs(pd.y - 2.2) < 0.000001, "pd.y must be 2.2");
+    assert(pd.x == 1.1, "pd.x must be 1.1");
+    assert(pd.y == 2.2, "pd.y must be 2.2");
 }
