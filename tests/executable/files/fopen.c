5997b74cccc82ea476074adf2a56b9fdf0cf8331
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2022-04-04 15:36:02 +0300

    Remove outdated

--- tests/executable/files/fopen.c
+++
@@ -1,7 +0,0 @@
-void main()
-{
-	//FILE f;
-	double f = 0;
-
-	f = fopen("/Users/ant/Desktop/Reutov/Reutov/frame3.bmp");
-}
--- tests/executable/files/fscanf.c
+++
@@ -1,9 +0,0 @@
-void main()
-{
-	double f = 0;
-	f = fopen("/Users/ant/Desktop/Reutov/Reutov/frame3.bmp");
-
-	char ch = fgetc(f);
-	printf("ch = %c", ch);
-	
-}
