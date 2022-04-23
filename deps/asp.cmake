466253710b8259fc577830658d013402381c9e36
Maxim Menshikov <maxim@menshikov.org>
2022-04-23 16:01:34 +0300

    Add a method of adding local ASP

--- deps/asp.cmake
+++ deps/asp.cmake
@@ -3,10 +3,14 @@ cmake_minimum_required(VERSION 3.14 FATAL_ERROR)
 
 include(FetchContent)
 
-FetchContent_Declare(
-  asp
-  GIT_TAG "simple_api"
-  GIT_REPOSITORY "https://github.com/maximmenshikov/asp.git"
-)
+if (DEFINED ENABLE_ASP_LOCAL)
+  add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/asp)
+else ()
+  FetchContent_Declare(
+    asp
+    GIT_TAG "simple_api"
+    GIT_REPOSITORY "https://github.com/maximmenshikov/asp.git"
+  )
 
-FetchContent_MakeAvailable(asp)
+  FetchContent_MakeAvailable(asp)
+endif ()
