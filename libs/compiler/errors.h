046f91263a421358e1eaa34a7d937114cf2580f1
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2022-10-07 14:01:48 +0300

    Enable new locator

--- libs/compiler/errors.h
+++ libs/compiler/errors.h
@@ -165,7 +165,7 @@ typedef enum WARNING
  *	@param	io			Universal io
  *	@param	num			Error number
  */
-void error(const universal_io *const io, err_t num, ...);
+void error(universal_io *const io, err_t num, ...);
 
 /**
  *	Emit a warning for some problem
@@ -173,7 +173,7 @@ void error(const universal_io *const io, err_t num, ...);
  *	@param	io			Universal io
  *	@param	num			Warning number
  */
-void warning(const universal_io *const io, warning_t num, ...);
+void warning(universal_io *const io, warning_t num, ...);
 
 
 /**
@@ -183,7 +183,7 @@ void warning(const universal_io *const io, warning_t num, ...);
  *	@param	num			Error number
  *	@param	args		Variable list
  */
-void verror(const universal_io *const io, const err_t num, va_list args);
+void verror(universal_io *const io, const err_t num, va_list args);
 
 /**
  *	Emit a warning (embedded version)
@@ -192,7 +192,7 @@ void verror(const universal_io *const io, const err_t num, va_list args);
  *	@param	num			Warning number
  *	@param	args		Variable list
  */
-void vwarning(const universal_io *const io, const warning_t num, va_list args);
+void vwarning(universal_io *const io, const warning_t num, va_list args);
 
 
 /**
