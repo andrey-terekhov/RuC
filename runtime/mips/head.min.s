dcdb2e5827a879a9ebda4822a7524e12597385ac
Georgiy Belyanin <belyaningeorge@ya.ru>
2023-02-13 14:46:41 +0300

    Add support for runtime resources in CMake.

---
+++ runtime/mips/head.min.s
@@ -0,0 +1,15 @@
+    .section .mdebug.abi32
+    .previous
+    .nan    legacy;
+    .module fp=xx
+    .module nooddspreg
+    .abicalls
+    .option pic0
+    .text
+    .align 2
+    .globl main
+    .ent main
+    .type main, @function
+main:
+    lui $gp, %%hi(__gnu_local_gp)
+    addiu $gp, $gp, %%lo(__gnu_local_gp)
