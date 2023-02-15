ba465224bc279ca4e1011c41b841cc3eac357c6d
Georgiy Belyanin <belyaningeorge@ya.ru>
2023-02-15 14:14:24 +0300

    Update minified runtime/mips files.

--- runtime/mips/foot.min.s
+++ runtime/mips/foot.min.s
@@ -1,3 +1,89 @@
+FUNC2:
+	sw $ra, -4($sp)
+	addi $sp, $sp, -4
+	jal assert
+	addi $sp, $sp, 4
+	lw $ra, -4($sp)
+	jr $ra
+FUNC6:
+	sw $ra, -4($sp)
+	addi $sp, $sp, -4
+	jal asin
+	addi $sp, $sp, 4
+	lw $ra, -4($sp)
+	jr $ra
+FUNC10:
+	sw $ra, -4($sp)
+	addi $sp, $sp, -4
+	jal cos
+	addi $sp, $sp, 4
+	lw $ra, -4($sp)
+	jr $ra
+FUNC14:
+	sw $ra, -4($sp)
+	addi $sp, $sp, -4
+	jal sin
+	addi $sp, $sp, 4
+	lw $ra, -4($sp)
+	jr $ra
+FUNC18:
+	sw $ra, -4($sp)
+	addi $sp, $sp, -4
+	jal sin
+	addi $sp, $sp, 4
+	lw $ra, -4($sp)
+	jr $ra
+FUNC22:
+	sw $ra, -4($sp)
+	addi $sp, $sp, -4
+	jal log
+	addi $sp, $sp, 4
+	lw $ra, -4($sp)
+	jr $ra
+FUNC26:
+	sw $ra, -4($sp)
+	addi $sp, $sp, -4
+	jal log10
+	addi $sp, $sp, 4
+	lw $ra, -4($sp)
+	jr $ra
+FUNC30:
+	sw $ra, -4($sp)
+	addi $sp, $sp, -4
+	jal sqrt
+	addi $sp, $sp, 4
+	lw $ra, -4($sp)
+	jr $ra
+FUNC34:
+	sw $ra, -4($sp)
+	addi $sp, $sp, -4
+	jal rand
+	addi $sp, $sp, 4
+	lw $ra, -4($sp)
+	jr $ra
+FUNC38:
+	sw $ra, -4($sp)
+	addi $sp, $sp, -4
+	jal round
+	addi $sp, $sp, 4
+	lw $ra, -4($sp)
+	jr $ra
+FUNC42:
+	jr $ra
+FUNC46:
+	jr $ra
+FUNC50:
+	jr $ra
+FUNC54:
+	jr $ra
+FUNC58:
+	jr $ra
+FUNC62:
+	jr $ra
+FUNC66:
+	jr $ra
+FUNC70:
+	jr $ra
 DEFARR1:
 	sw $a1, 4($a0)
 	li $v0, 4
--- runtime/mips/head.min.s
+++ runtime/mips/head.min.s
@@ -7,6 +7,15 @@
 	.option pic0
 	.text
 	.align 2
+	.extern assert
+	.extern asin
+	.extern cos
+	.extern sin
+	.extern exp
+	.extern log
+	.extern log10
+	.extern sqrt
+	.extern round
 	.globl main
 	.ent main
 	.type main, @function
