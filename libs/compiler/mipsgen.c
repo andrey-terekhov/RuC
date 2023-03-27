9640c8ae5cd0b6a4bf11e1f22b9bffa3d25d1ed7
IgnatSergeev/Desktop <ignatii.f.sergeev@gmail.com>
2023-03-28 00:23:24 +0300

    Changes for easy merge

--- libs/compiler/mipsgen.c
+++ libs/compiler/mipsgen.c
@@ -2142,7 +2142,6 @@ static rvalue emit_builtin_call(encoder *const enc, const node *const nd)
  */
 static rvalue emit_function_argument(encoder *const enc, const node *const arg)
 {
-	const rvalue tmp = emit_expression(enc, arg);
 	const type_t arg_type = expression_get_type(arg);
 
 	if (type_is_structure(enc->sx, arg_type))
@@ -2151,8 +2150,7 @@ static rvalue emit_function_argument(encoder *const enc, const node *const arg)
 		enc->scope_displ += mips_type_size(enc->sx, arg_type);
 		enc->max_displ = max(enc->scope_displ, enc->max_displ);
 
-		emit_struct_assignment(enc, &argument_copy, arg);
-		return emit_load_of_lvalue(enc, &argument_copy);
+		return emit_struct_assignment(enc, &argument_copy, arg);
 	}
 
 	if (type_is_function(enc->sx, arg_type) && type_is_structure(enc->sx, type_function_get_return_type(enc->sx, arg_type)))
@@ -2165,6 +2163,7 @@ static rvalue emit_function_argument(encoder *const enc, const node *const arg)
 		return emit_load_of_lvalue(enc, &function_return_value);
 	}
 
+	const rvalue tmp = emit_expression(enc, arg);
 	return (tmp.kind == RVALUE_KIND_CONST) ? emit_load_of_immediate(enc, &tmp) : tmp;
 }
 
