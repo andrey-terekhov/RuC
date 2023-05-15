153a17d593bcfce56284e75bec208c48035ff59d
IgnatSergeev <ignatii.f.sergeev@gmail.com>
2023-05-15 21:52:36 +0300

    Extended arrays initialization tests

--- tests/codegen/executable/arrays/define/arrays_initialization_with_arrays_inside_initializators.c
+++ tests/codegen/executable/arrays/define/arrays_initialization_with_arrays_inside_initializators.c
@@ -1,10 +1,14 @@
 void main()
 {
-    int i, j;
-    int  a[3] = {1, 2, 3};
-    int  b[2][3] =  {a, {4, 5, 6}};
-    int  c[2][2][3] = {{{1, 2, 3}, {4, 5, 6}}, b};
-	int  d[3][2][2][] = {c, c, c};
+    int a[3] = {1, 2, 3};
+    int b[2][3] =  {a, {4, 5, 6}};
+    int c[2][2][3] = {{{1, 2, 3}, {4, 5, 6}}, b};
+	int d[3][2][2][] = {c, c, c};
+	int e[2][3] = {b[0], b[1]};
+	float f[] = {1, 2, 3};
+	float g[2][3] = {{2, 3 ,4}, {5, 6 ,7}};
+	float h[3][3] = {f, g[0], g[1]};
+	const float delta = 0.001;
 
     assert(a[0] == 1, "a[0] must be 1");
     assert(a[1] == 2, "a[1] must be 2");
@@ -68,4 +72,32 @@ void main()
 	assert(d[2][1][1][0] == 4, "d[2][1][1][0] must be 4");
 	assert(d[2][1][1][1] == 5, "d[2][1][1][1] must be 5");
 	assert(d[2][1][1][2] == 6, "d[2][1][1][2] must be 6");
+
+	assert(e[0][0] == 1, "b[0][0] must be 1");
+	assert(e[0][1] == 2, "b[0][1] must be 2");
+	assert(e[0][2] == 3, "b[0][2] must be 3");
+	assert(e[1][0] == 4, "b[1][0] must be 4");
+	assert(e[1][1] == 5, "b[1][1] must be 5");
+	assert(e[1][2] == 6, "b[1][2] must be 6");
+
+	assert(abs(a[0] - 1) < delta, "a[0] must be 1");
+	assert(abs(a[1] - 2) < delta, "a[1] must be 2");
+	assert(abs(a[2] - 3) < delta, "a[2] must be 3");
+
+	assert(abs(g[0][0] - 2) < delta, "g[0][0] must be 2");
+	assert(abs(g[0][1] - 3) < delta, "g[0][1] must be 3");
+	assert(abs(g[0][2] - 4) < delta, "g[0][2] must be 4");
+	assert(abs(g[1][0] - 5) < delta, "g[1][0] must be 5");
+	assert(abs(g[1][1] - 6) < delta, "g[1][1] must be 6");
+	assert(abs(g[1][2] - 7) < delta, "g[1][2] must be 7");
+
+	assert(abs(h[0][0] - 1) < delta, "h[0][0] must be 1");
+	assert(abs(h[0][1] - 2) < delta, "h[0][1] must be 2");
+	assert(abs(h[0][2] - 3) < delta, "h[0][2] must be 3");
+	assert(abs(h[1][0] - 2) < delta, "h[1][0] must be 2");
+	assert(abs(h[1][1] - 3) < delta, "h[1][1] must be 3");
+	assert(abs(h[1][2] - 4) < delta, "h[1][2] must be 4");
+	assert(abs(h[2][0] - 5) < delta, "h[1][0] must be 5");
+	assert(abs(h[2][1] - 6) < delta, "h[1][1] must be 6");
+	assert(abs(h[2][2] - 7) < delta, "h[1][2] must be 7");
 }
