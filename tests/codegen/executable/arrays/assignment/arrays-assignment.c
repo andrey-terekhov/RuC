fff8ef9cea787f48f33f993937eef482193ac75d
IgnatSergeev <ignatii.f.sergeev@gmail.com>
2023-05-15 19:50:59 +0300

    Updated assignment test

--- tests/codegen/executable/arrays/assignment/arrays-assignment.c
+++ tests/codegen/executable/arrays/assignment/arrays-assignment.c
@@ -38,5 +38,41 @@ void main()
 	assert(c[1][1][0] == 2, "c[1][1][0] must be 1");
 	assert(c[1][1][1] == 3, "c[1][1][1] must be 2");
 	assert(c[1][1][2] == 4, "c[1][1][2] must be 3");
+
+	a = b[1];
+
+	assert(a[0] == 1, "a[0] must be 1");
+	assert(a[1] == 2, "a[1] must be 2");
+	assert(a[2] == 3, "a[2] must be 3");
+
+	a = c[0][0];
+
+	assert(a[0] == 7, "a[0] must be 7");
+	assert(a[1] == 8, "a[1] must be 8");
+	assert(a[2] == 9, "a[2] must be 9");
+
+	b[0] = c[0][0];
+
+	assert(b[0][0] == 7, "b[0][0] must be 7");
+	assert(b[0][1] == 8, "b[0][1] must be 8");
+	assert(b[0][2] == 9, "b[0][2] must be 9");
+	assert(b[1][0] == 1, "b[1][0] must be 1");
+	assert(b[1][1] == 2, "b[1][1] must be 2");
+	assert(b[1][2] == 3, "b[1][2] must be 3");
+
+	c[0] = {b[1], a};
+
+	assert(c[0][0][0] == 1, "c[0][0][0] must be 1");
+	assert(c[0][0][1] == 2, "c[0][0][1] must be 2");
+	assert(c[0][0][2] == 3, "c[0][0][2] must be 3");
+	assert(c[0][1][0] == 7, "c[0][1][0] must be 7");
+	assert(c[0][1][1] == 8, "c[0][1][1] must be 8");
+	assert(c[0][1][2] == 9, "c[0][1][2] must be 9");
+	assert(c[1][0][0] == 2, "c[1][0][0] must be 2");
+	assert(c[1][0][1] == 3, "c[1][0][1] must be 3");
+	assert(c[1][0][2] == 4, "c[1][0][2] must be 4");
+	assert(c[1][1][0] == 2, "c[1][1][0] must be 1");
+	assert(c[1][1][1] == 3, "c[1][1][1] must be 2");
+	assert(c[1][1][2] == 4, "c[1][1][2] must be 3");
 }
 
